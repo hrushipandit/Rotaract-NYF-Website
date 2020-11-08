0e8a6c115fc451831cdeaf559ecd52222a5cd350
Divya Talera <dtdivya47@gmail.com>
2020-11-08 19:10:52 +0530

    major changes

--- member.php
+++ member.php
@@ -2,7 +2,64 @@
     include 'header.php';
 ?>
 
+<div id="team">
+    <div class="section-header">
+        <h3 class="section-title"> Committee</h3>
+        <br>
+        <h2 style="text-align:center;"><b>E-services committee</b></h2><br>
+        <div class="d-flex justify-content-center">
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="image-flip">
+                    <div class="mainflip flip-0">
+                        <div class="frontside">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                <p><img class=" img-fluid" src="assets/img/e-services.jpg" alt="card image"></p>
+                                    <h4 class="card-title">Head <br>Hrushi Pandit</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="backside">
+                            <div class="card">
+                                <div class="card-body text-center mt-4">
+                                <p><img class=" img-fluid" src="assets/img/e-services head.png" alt="card image"></p>
+                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                                    Quidem aperiam, id earum laboriosam adipisci ipsum eaque ut sit recusandae. Quam mollitia, saepe officiis
+                                        culpa atque tenetur quidem suscipit alias laboriosam?</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="image-flip">
+                    <div class="mainflip flip-0">
+                        <div class="frontside">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                <p><img class=" img-fluid" src="assets/img/e-services.jpg" alt="card image"></p>
+                                    <h4 class="card-title"> Co-head <br> Divya Talera</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="backside">
+                            <div class="card">
+                                <div class="card-body text-center mt-4">
+                                <p><img class=" img-fluid" src="assets/img/e-services co-head.png" alt="card image"></p>
+                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                    Praesentium corporis, iste voluptates officiis quidem ipsum laborum, quaerat dolore ut soluta, sapiente 
+                                    veritatis commodi! Dolorem rerum eveniet inventore ipsa! Eum, in.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <?php
